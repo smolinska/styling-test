7b96d394d5f5aa4e758c51cfc6c91debc48d2b8a
smolinska <smolinska.ps@gmail.com>
2020-05-14 22:27:35 +0200

    add open street map, change breakpoints

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare const ol: any;
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   constructor() {}
+  latitude: number = 52.2296756;
+  longitude: number = 21.0122287;
+
+  map: any;
+
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
+        zoom: 10
+      })
+    });
+  }
 }
